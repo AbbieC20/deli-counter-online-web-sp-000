21f3c88b7cbe550b9d4ec0f22c80e5d9f97e2993
remote-torrent-6747 <abbyc@sky.com>
2020-12-17 19:21:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(queue)
+  if queue.length == 0
+    puts "The line is currently empty."
+  else
+    queue_list = queue.join(", ")
+    puts "The line is currently: #{queue_list}"
+  end
+end
+
+#queue is the array which shows people in the deli queue
+
+#def take_a_number(queue, last)
+#  puts "Welcome, #{last}. You are number #{queue + 1} in line."
+#end
+
+#queue is the array for people in the deli queue
+#last is the name of the person joining the end of the line
