156fb83f23d843a9160a313ab3af9be751677369
remote-torrent-6747 <abbyc@sky.com>
2020-12-17 19:46:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(queue)
       position = "#{position + 1}. #{name}"
       position_list.push(position)
     end
-    puts "Hello" position_list
+    puts "Hello" + position_list.join
   end
 end
 
