8be3d723850ede9d812ffba8275cbfb06ff60896
remote-torrent-6747 <abbyc@sky.com>
2020-12-17 20:32:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,7 @@ end
 
 def take_a_number (katz_deli, name)
   katz_deli.push(name)
-
-  puts "Welcome, #{name}. You are number 1 in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 #queue is the array for people in the deli queue
