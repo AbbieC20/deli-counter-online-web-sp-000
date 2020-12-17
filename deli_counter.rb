53744aa0f44e537dbdda04bd041acb3399ad4478
remote-torrent-6747 <abbyc@sky.com>
2020-12-17 19:49:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,15 +12,9 @@ def line(queue)
 end
 
 
-
-#queue_list = queue.join(", ")
-#puts "The line is currently: #{queue_list}"
-
-#queue is the array which shows people in the deli queue
-
-#def take_a_number(queue, last)
-#  puts "Welcome, #{last}. You are number #{queue + 1} in line."
-#end
+def take_a_number(queue, last)
+  puts "Welcome, #{last}. You are number #{queue + 1} in line."
+end
 
 #queue is the array for people in the deli queue
 #last is the name of the person joining the end of the line
